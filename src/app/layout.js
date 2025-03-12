bb691683a121a5cc1caaabb3e01c8ff8e21b61c7
briskking <iambriskking@gmail.com>
2025-03-12 17:20:09 +0100

    feat: installed preferred font from local directory for implementation on project design

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// const electro regular = ({
+
+// })
+
+export const metadata = {
+  title: "ballAZZ Events Spot",
+  description: "Best Events spot in +234",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+<header class="bg-purple-700 max-w- (1440px) mx-auto">
+
+    <div>
+      <h1>b{"\u25B2"}llAZZ
+    Your Deluxe NIght Spot(Turn It Up.)</h1>
+    </div>
+    
+
+</header>
+
+
+    <div>
+      UPCOMING EVENTS
+    Experience elegance with a lineup that’s going to leave you flabbergasted everyday only at Hammerzz the Luxury Nightclub.
+
+    Experience a revolution in the nightlife and entertainment scene that has transformed Goa’s clubbing landscape. Hammerzz Nightclub stands as the epitome of extravagant revelry, offering an unparalleled party experience in the heart of North Goa. As the best nightclub in Goa, we pride ourselves on curating unforgettable nights filled with pulsating beats, captivating lights, and an electrifying ambiance that will leave you craving for more.
+
+Situated in the vibrant landscape of North Goa, Hammerzz Nightclub has earned its reputation as the most famous nightclub in Goa. We have redefined the party experience, setting new standards of opulence and excitement. From the moment you step through our doors, you’ll be enveloped in an atmosphere of sheer luxury and unbridled energy.
+
+At Hammerzz, we believe in creating moments that last a lifetime. Our state-of-the-art sound systems and world-class DJs ensure that every beat resonates deep within your soul. The carefully designed lighting setup elevates the entire experience, casting an enchanting glow that sets the mood for an unforgettable night. Whether you’re dancing the night away with friends or sipping on handcrafted cocktails at our exquisite bar, every moment at Hammerzz is a testament to our commitment to excellence.
+
+Indulge in the epitome of nightlife luxury, and become a part of the legend that is Hammerzz Nightclub. Don’t miss out on what is undoubtedly the best nightclub in North Goa. Join us for an extraordinary evening of music, dance, and memories that will linger long after the night is over. Welcome to the revolution; welcome to Hammerzz!
+
+To learn more, contact Hammerzz The Luxury Nightclub
+    
+    
+    Hammerzz Nightclub goa, Calangute - Baga Rd, Baga, Goa pincode: 403516, India
+    +919112211492 +919112211493
+    events@hammerzz.com sales@hammerzz.com
+    Opening Time
+    Opening Hours - 9.00 pm onwards
+    Party Hours - 10.00 pm onwards
+    Open all days
+    Quick Links
+    About
+    Booking
+    Gallery
+    Event
+    Blogs
+    Contact</div>
+    </div>
+  )
+}
+
+export default Home
+
+// npx create-next-app@latest .
